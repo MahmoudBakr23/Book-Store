a2edf9d5f19bdf447d2bce2212e423cd106e9617
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 21:10:28 +0200

    Setup: publisher model and table

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,2 +1,4 @@
 class Publisher < ApplicationRecord
+    has_many :books, dependent: :destroy
+    validates :name, presence: true, uniqueness: true, length: {minimum:3, maximum:15}
 end
--- db/migrate/20201013190657_create_publishers.rb
+++ db/migrate/20201013190657_create_publishers.rb
@@ -1,6 +1,7 @@
 class CreatePublishers < ActiveRecord::Migration[5.2]
   def change
     create_table :publishers do |t|
+      t.string :name
 
       t.timestamps
     end
