6a84e7add27c32bf3ee6ade94c9e06f9a0965fe0
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 18:30:47 +0200

    generate: books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/new'
+  get 'books/create'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
   get 'about/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
