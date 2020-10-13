1f60f06d71fd02d9ea9540611f1ee8372bd46769
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 21:41:42 +0200

    Modify: models associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    has_many :books, dependent: :destroy
+    has_many :books, class_name: "Book", foreign_key: "category_id", dependent: :destroy
     validates :name, presence: true, uniqueness: true, length: {minimum:3}
 end
