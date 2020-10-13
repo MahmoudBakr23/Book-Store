14e4e98e6020c99e7d9d73118a4ec412e12943e0
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 21:15:20 +0200

    Add: columns to paperclip fields in book tables

--- db/migrate/20201013191357_add_paperclip_fields_to_books.rb
+++ db/migrate/20201013191357_add_paperclip_fields_to_books.rb
@@ -1,4 +1,8 @@
 class AddPaperclipFieldsToBooks < ActiveRecord::Migration[5.2]
   def change
+    add_column :books, :image_file_name, :string
+    add_column :books, :image_content_type, :string
+    add_column :books, :image_file_size, :integer
+    add_column :books, :image_updated_at, :datetime
   end
 end
