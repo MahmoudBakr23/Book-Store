68ac76c0be18993265f66f16a9d1d5d8ced2c179
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 21:02:56 +0200

    Migrate authors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_185342) do
+ActiveRecord::Schema.define(version: 2020_10_13_190038) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
