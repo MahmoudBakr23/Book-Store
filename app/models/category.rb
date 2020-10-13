204cf4d6b24ad255ac14ae1c1edb65a93267ad2e
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 20:55:13 +0200

    Update: categories model and table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :books, dependent: :destroy
+    validates :name, presence: true, uniqueness: true, length: {minimum:3}
 end
--- db/migrate/20201013185342_create_categories.rb
+++ db/migrate/20201013185342_create_categories.rb
@@ -1,6 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
+      t.string :name
 
       t.timestamps
     end
