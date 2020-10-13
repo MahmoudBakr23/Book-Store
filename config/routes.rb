5a888251a5aa6f6b67a2edda64c3ebd0504a69fa
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 21:17:25 +0200

    Modify: routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,10 @@
 Rails.application.routes.draw do
-  get 'publishers/index'
-  get 'publishers/new'
-  get 'publishers/create'
-  get 'publishers/show'
-  get 'publishers/edit'
-  get 'publishers/update'
-  get 'publishers/destroy'
-  get 'authors/index'
-  get 'authors/new'
-  get 'authors/create'
-  get 'authors/show'
-  get 'authors/edit'
-  get 'authors/update'
-  get 'authors/destroy'
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/show'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-  get 'books/index'
-  get 'books/new'
-  get 'books/create'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'about/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about/index', to: 'about#index'
+  
+  root 'books#index'
+
+  resources :books
+  resources :categories
+  resources :authors
+  resources :publishers
 end
