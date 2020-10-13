0385006f8ff87cd2092e308bb24e1a8ef1f86a05
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-13 21:01:55 +0200

    Setup: author model and table

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    has_many :books, dependent: :destroy
+    validates :first_name, presence: true, length: {minimum:3, maximum:18}
+    validates :last_name, presence: true, uniqueness: true, length: {minimum:3, maximum:18}
 end
--- db/migrate/20201013190038_create_authors.rb
+++ db/migrate/20201013190038_create_authors.rb
@@ -1,7 +1,9 @@
 class CreateAuthors < ActiveRecord::Migration[5.2]
   def change
     create_table :authors do |t|
-
+      t.string :first_name
+      t.string :last_name
+      
       t.timestamps
     end
   end
